f2beea131eeb975454cd7c50705f7266d2adc50c
Karma63 <adam2004buchan@outlook.com>
2023-12-13 22:58:44 +0000

    the chat is now able to be outputted to the users

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
 const path = require('path');
 
 const app = express();
@@ -8,14 +10,40 @@ const port = 3000;
 app.use('/styles', express.static(path.join(__dirname, 'styles')));
 app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
 
+
+// Create an HTTP server and integrate it with Express
+const server = http.createServer(app);
+
+// Create a WebSocket server by passing the HTTP server
+const wss = new WebSocket.Server({ server });
+
 // Set the 'views' folder as the static directory
 app.use(express.static(path.join(__dirname, 'views')));
 
-// Define a route for the home page
+// Serve the 'index.html' file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(port, () => {
+// WebSocket server handling connections
+wss.on('connection', (ws) => {
+    // WebSocket connection is established
+    console.log('WebSocket connection established');
+
+    // Handle incoming messages from clients
+    ws.on('message', (message) => {
+        console.log(`Received message: ${message}`);
+
+        // Broadcast the message to all connected clients
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+});
+
+// Start the server and listen on the specified port
+server.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
--- scripts/chat.js
+++ scripts/chat.js
@@ -1,3 +1,33 @@
+// chat.js
+
+// Create a WebSocket connection to the server
+const socket = new WebSocket('ws://localhost:3000/Chat.html');
+
+// Connection opened
+socket.addEventListener('open', (event) => {
+    console.log('WebSocket connection opened');
+});
+
+// Listen for messages from the server
+socket.addEventListener('message', (event) => {
+    // Assuming the server sends JSON messages
+    const messageData = JSON.parse(event.data);
+    console.log('Received message from server:', messageData);
+
+    // Update your UI or perform actions based on the received message
+    // For example, append the message to the list of messages
+    const messagesList = document.getElementById('messages');
+    const li = document.createElement('li');
+    li.textContent = `${messageData.username}: ${messageData.message}`;
+    messagesList.appendChild(li);
+});
+
+// Connection closed
+socket.addEventListener('close', (event) => {
+    console.log('WebSocket connection closed');
+});
+
+// Handle form submission
 $(document).ready(function () {
     // Get the username from the URL query parameter
     var urlParams = new URLSearchParams(window.location.search);
@@ -5,7 +35,7 @@ $(document).ready(function () {
 
     // Use the username as needed
     console.log('Username:', username);
-    $('#username').val(username)
+    $('#username').val(username);
 
     $('#form').submit(function (e) {
         e.preventDefault(); // Prevent the form from submitting
@@ -14,7 +44,11 @@ $(document).ready(function () {
         var username = $('#username').val();
 
         if (message.trim() && username) {
-            // Your logic to send the message
+            // Assuming you want to send JSON messages to the server
+            const messageData = { username, message };
+
+            // Send the message to the server
+            socket.send(JSON.stringify(messageData));
 
             // After sending the message, clear the input field but keep the username
             $('#input').val('');
@@ -23,7 +57,9 @@ $(document).ready(function () {
             $('#input').focus();
         }
     });
+
+    // Redirect to login page if the username is invalid
     if (username == null || username == "" || username.includes(" ") == true) {
-        window.location.href = 'login.html'
+        window.location.href = 'login.html';
     }
 });
