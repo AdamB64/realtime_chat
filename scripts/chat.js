7c5eaeab095ef602cc03fedf0c8280ada466cceb
Karma63 <adam2004buchan@outlook.com>
2023-12-14 00:01:53 +0000

    the message isnt displayed

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,41 +1,43 @@
-// chat.js
-
-// Create a WebSocket connection to the server
-const socket = new WebSocket('ws://localhost:3000/Chat.html');
-
-// Connection opened
-socket.addEventListener('open', (event) => {
-    console.log('WebSocket connection opened');
-});
-
-// Listen for messages from the server
-socket.addEventListener('message', (event) => {
-    // Assuming the server sends JSON messages
-    const messageData = JSON.parse(event.data);
-    console.log('Received message from server:', messageData);
-
-    // Update your UI or perform actions based on the received message
-    // For example, append the message to the list of messages
-    const messagesList = document.getElementById('messages');
-    const li = document.createElement('li');
-    li.textContent = `${messageData.username}: ${messageData.message}`;
-    messagesList.appendChild(li);
-});
-
-// Connection closed
-socket.addEventListener('close', (event) => {
-    console.log('WebSocket connection closed');
-});
-
-// Handle form submission
 $(document).ready(function () {
-    // Get the username from the URL query parameter
-    var urlParams = new URLSearchParams(window.location.search);
-    var username = urlParams.get('username');
+    var socket = new WebSocket('ws://localhost:3000');
 
-    // Use the username as needed
-    console.log('Username:', username);
-    $('#username').val(username);
+    // WebSocket connection opened
+    socket.addEventListener('open', (event) => {
+        console.log('WebSocket connection opened', event);
+        // You can add any initialization logic here
+    });
+
+    // WebSocket connection closed
+    socket.addEventListener('close', (event) => {
+        console.log('WebSocket connection closed', event);
+    });
+
+    // WebSocket connection error
+    socket.addEventListener('error', (event) => {
+        console.error('WebSocket connection error', event);
+    });
+
+    // WebSocket received a message
+    socket.addEventListener('message', (event) => {
+        console.log("message")
+        console.log('Raw message:', event.data);
+
+        try {
+            var data = JSON.parse(event.data);
+            console.log('Parsed message:', data);
+
+            // Assuming data has a 'username' and 'message' property
+            if (data.username && data.message) {
+                // Append the message to the #messages ul
+                $('#messages').append('<li><strong>' + data.username + ':</strong> ' + data.message + '</li>');
+
+                // Optionally, scroll to the bottom of the #messages ul
+                $('#messages').scrollTop($('#messages')[0].scrollHeight);
+            }
+        } catch (error) {
+            console.error('Error parsing message:', error);
+        }
+    });
 
     $('#form').submit(function (e) {
         e.preventDefault(); // Prevent the form from submitting
@@ -44,22 +46,14 @@ $(document).ready(function () {
         var username = $('#username').val();
 
         if (message.trim() && username) {
-            // Assuming you want to send JSON messages to the server
-            const messageData = { username, message };
-
             // Send the message to the server
-            socket.send(JSON.stringify(messageData));
+            socket.send(JSON.stringify({ username: username, message: message }));
 
-            // After sending the message, clear the input field but keep the username
+            // Clear the input field
             $('#input').val('');
 
-            // Optionally, you can also focus back on the input field for the next message
+            // Optionally, focus back on the input field for the next message
             $('#input').focus();
         }
     });
-
-    // Redirect to login page if the username is invalid
-    if (username == null || username == "" || username.includes(" ") == true) {
-        window.location.href = 'login.html';
-    }
 });
