f4c6fb644abf8612e7e97b8709904e6515530d1c
Karma63 <adam2004buchan@outlook.com>
2023-12-22 10:02:33 +0000

    Add Konami Code functionality to home and start pages for tetris

--- scripts/home.js
+++ scripts/home.js
@@ -118,4 +118,26 @@ $(document).ready(async function () {
             });
     });
 
+    const konamiCode = ['arrowup', 'arrowup', 'arrowdown', 'arrowdown', 'arrowleft', 'arrowright', 'arrowleft', 'arrowright', 'b', 'a'];
+    let konamiCodeIndex = 0;
+
+    function checkKonamiCode(event) {
+        const key = event.key.toLowerCase();
+        //console.log(key);
+        if (key === konamiCode[konamiCodeIndex]) {
+            konamiCodeIndex++;
+
+            if (konamiCodeIndex === konamiCode.length) {
+                // Konami Code successfully entered, redirect to Tetris.html
+                console.log('Konami Code entered!');
+                window.location.href = 'Tetris.html';
+            }
+        } else {
+            // Reset the code index if a wrong key is pressed
+            konamiCodeIndex = 0;
+        }
+    }
+
+    document.addEventListener('keydown', checkKonamiCode);
+
 });
--- scripts/start.js
+++ scripts/start.js
@@ -12,4 +12,26 @@ $(document).ready(function () {
         window.location.href = "Login.html";
         //console.log("Login")
     });
+
+    const konamiCode = ['arrowup', 'arrowup', 'arrowdown', 'arrowdown', 'arrowleft', 'arrowright', 'arrowleft', 'arrowright', 'b', 'a'];
+    let konamiCodeIndex = 0;
+
+    function checkKonamiCode(event) {
+        const key = event.key.toLowerCase();
+        //console.log(key);
+        if (key === konamiCode[konamiCodeIndex]) {
+            konamiCodeIndex++;
+
+            if (konamiCodeIndex === konamiCode.length) {
+                // Konami Code successfully entered, redirect to Tetris.html
+                console.log('Konami Code entered!');
+                window.location.href = 'Tetris.html';
+            }
+        } else {
+            // Reset the code index if a wrong key is pressed
+            konamiCodeIndex = 0;
+        }
+    }
+
+    document.addEventListener('keydown', checkKonamiCode);
 });
